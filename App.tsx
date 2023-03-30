42bc51aee48330fef3e292d03a6446d620bbc82d
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2023-03-30 09:11:30 -0300

    feat: project started

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Signin from './src/modules/Authentication/SignIn';
+
+const App: React.FC = () => {
+  return (
+    <Signin />
+  );
+}
+
+export default App;
---
+++ src/modules/Authentication/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Tile } from './styles';
+
+const Signin: React.FC = () => {
+  return (
+    <Container>
+      <Tile>Bem vindo ao instagram</Tile>
+    </Container>
+  );
+}
+
+export default Signin;
